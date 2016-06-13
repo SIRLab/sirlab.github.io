0a0217c8e9498432aabd89fc6f3e2cb2742692d9
johnathan <johnathanfercher22@gmail.com>
2016-06-13 01:00:35 -0300

    update documentations

---
+++ assets/docs/doc_viewer/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['handleposition',['handlePosition',['../d5/d5a/classGraphics.html#a60bac80cfd3b0fc45b35deaa44fea63d',1,'Graphics']]]
+];
---
+++ assets/docs/doc_vision/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['changewindowsize',['changeWindowSize',['../d5/d5a/classGraphics.html#a78ef6a0b1f6c142c9d650ffddfbaa2ed',1,'Graphics']]]
+];
